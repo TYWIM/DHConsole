ddbfd3f2efdb8e3a2182845c454a9bfaeaac071f
承心 <117726202+TYWIM@users.noreply.github.com>
2025-03-11 21:43:32 +0800

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// 存储键
+export const HOST_STORAGE_KEY = 'muip-host';
+export const PORT_STORAGE_KEY = 'muip-port';
+export const USE_SSL_STORAGE_KEY = 'muip-use-ssl';
+export const ADMIN_KEY_STORAGE_KEY = 'muip-admin-key';
+export const DEFAULT_LANGUAGE = 'zh_CN';
